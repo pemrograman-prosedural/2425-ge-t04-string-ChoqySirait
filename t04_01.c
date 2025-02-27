df9a636d2344edc3f6e9f9abd5f313c0fe3320e8
Choqy Pananda Sirait <krisky325@gmail.com>
2025-02-27 14:13:35 +0700

    Pair-25

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,28 @@
-// NIM - Name
-// NIM - Name
+// 12S24012 - Choqy Pananda Sirait
+// 12S24050 - Ester Hasianna Nainggolan
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+      char input[60];
+      scanf("%60s", input);
+  
+      int panjangstring = strlen(input);
+  
+      if (panjangstring % 3 != 0) {
+          printf("Panjang Inputan String Bukan Kelipatan 3 !\n");
+          return 1;
+      }
+  
+      for (int i = 0; i < panjangstring; i = i + 3) {
+          char Penampung_Sementara[4] = {0}; 
+          strncpy(Penampung_Sementara, &input[i], 3);
+          int Nilai_ASCII = atoi(Penampung_Sementara);
+          printf("\n%c", Nilai_ASCII);
+      }
+  
 
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,20 @@
-// NIM - Name
-// NIM - Name
+// 12S24012 - Choqy Pananda Sirait
+// 12S24050 - Ester Hasianna Nainggolan
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
-}
+  
+      char input[20]; 
+      scanf("%20[^\n]", input); 
+  
+      int PanjangString = strlen(input);
+  
+      for (int i = 0; i < PanjangString; i++) {
+          printf("%03d", input[i]); 
+      }
+  
+      return 0;
+  }
+  
